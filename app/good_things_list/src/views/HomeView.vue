54dad1eacb9e2334ddb2f307cf9d69527f9b3eeb
yonai <yonai@crudist.jp>
2023-08-13 12:48:18 +0900

    Home画面に投稿画面追加

--- app/good_things_list/src/views/HomeView.vue
+++ app/good_things_list/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <PostComponent msg="good things post"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import PostComponent from '@/components/PostComponent.vue';
 
 export default Vue.extend({
   name: 'HomeView',
   components: {
-    HelloWorld,
-  },
+    PostComponent
+},
 });
 </script>
