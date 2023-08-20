df754a89d9c583a74b352bde567d2fc8c6ea19b9
yonai <yonai@crudist.jp>
2023-08-20 09:54:35 +0900

    ログイン画面のUI完成

--- app/good_things_list/src/views/LoginView.vue
+++ app/good_things_list/src/views/LoginView.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
-    <v-card>
+    <v-card width="400px" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="">ログイン</h1>
       </v-card-title>
-    </v-card>
     <v-card-text>
       <v-form>
-        <v-text-field label="ユーザ名" />
-        <v-text-field label="パスワード" />
+        <v-text-field v-model="email" prepend-icon="mdi-email" label="メールアドレス" />
+        <v-text-field v-model="password" v-bind:type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword" prepend-icon="mdi-lock" v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" label="パスワード" />
+        <v-card-actions>
+          <v-btn class="info" @click="submit">ログイン</v-btn>
+        </v-card-actions>
       </v-form>
     </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
@@ -19,12 +22,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export default {
-  data() {
+  name: "LoginView",
+  data(){
     return {
-      email: '',
-      password: ''
-    };
-  },
+    showPassword: false,
+    email: '',
+    password: '',
+  }},
   methods: {
     async login() {
       try {
@@ -36,6 +40,9 @@ export default {
         // エラー処理
         console.error('ログインエラー:', error.message);
       }
+    },
+    submit() {
+      console.log(this.email this.password);
     }
   }
 };
